605af04a5f3159f9c82dc6e69e434866228972e0
Robson Abreu <contatorobsonabreu@gmail.com>
2016-08-28 20:54:02 -0300

    version 1.0.1

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <unistd.h>
 
 #define CIMA 72
 #define BAIXO 80
@@ -8,48 +9,77 @@
 #define DIR 77
 
 #define ESC 27
-void gotoxy(int x, int y){
-     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD){x-1,y-1});
+void gotoxy(int x, int y)
+{
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),(COORD)
+    {
+        x-1,y-1
+    });
 }
 int main()
 {
+    int linha = 1;
+    int coluna = 1;
+    int ch;
+    int incrementoLinha = 0;
+    int incrementoColuna = 0;
+    gotoxy(1,1);
+    printf("*");
 
-      int linha = 1;
-      int coluna = 1;
-      int ch;
-      gotoxy(1,1);
-      printf("*");
-	  for(;;) // Loop infinito
-      {
-          if (kbhit())
-          {
-              ch = getch(); // Pega a tecla pressionada.
-              if (ch == ESC)
-              {
-                  break;
-              }
-              if (ch == 224)  // 224 é uma tecla estendida.
-              {
-                  ch = getch();
-                  switch(ch)
-                  {
-                      case CIMA:
-                               linha--;
-                               break;
-                      case BAIXO:
-                               linha++;
-                               break;
-                      case ESQ:
-                               coluna--;
-                               break;
-                      case DIR:
-                               coluna++;
-                               break;
-                  }
-                  gotoxy(coluna,linha);
-                  printf("*");
-              }
-          }
-      }
-      return 0;
+    for(;;) // Loop infinito.
+    {
+        if (kbhit())
+        {
+            ch = getch(); // Pega a tecla pressionada.
+            if (ch == ESC)
+            {
+                break;
+            }
+            if (ch == 224) // 224 é uma tecla estendida.
+            {
+                ch = getch();
+                incrementoColuna = 0;
+                incrementoLinha = 0;
+
+                switch(ch)
+                {
+                case CIMA:
+                    incrementoLinha = -1;
+                    break;
+                case BAIXO:
+                    incrementoLinha = 1;
+                    break;
+                case ESQ:
+                    incrementoColuna = -1;
+                    break;
+                case DIR:
+                    incrementoColuna = 1;
+                    break;
+                }
+            }
+        }
+
+        if (linha >= 25)
+        {
+            linha = 0;
+        }
+        else if (linha <= 0)
+        {
+            linha = 25;
+        }
+
+        if (coluna >= 80)
+        {
+            coluna = 0;
+        }
+        else if (coluna <= 0)
+        {
+            coluna = 80;
+        }
+
+        Sleep(100); // Faz o jogo não ficar rápido.
+        gotoxy(coluna+=incrementoColuna, linha+=incrementoLinha);
+        printf("*");
+    }
+    return 0;
 }
