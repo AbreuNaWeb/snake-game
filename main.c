d659092b7ba64b62dff6288b5c82be33d1245943
Robson Abreu <contatorobsonabreu@gmail.com>
2016-10-12 22:23:56 -0300

    version 1.0.3 - bugfix

--- main.c
+++ main.c
@@ -7,12 +7,11 @@
 #define BAIXO 80
 #define ESQ 75
 #define DIR 77
-
 #define ESC 27
 
 signed char linha = 1;
 signed char coluna = 1;
-unsigned char guardaPosicao[80][25];
+unsigned char guardaPosicao[80][25] = {{0}};
 
 void gotoxy(int x, int y)
 {
@@ -24,46 +23,30 @@ void gotoxy(int x, int y)
 
 void antiEscape() // Esse método faz a cobra não "escapar" da tela.
 {
-    if (linha >= 25)
+    if (linha == 25)
     {
         linha = 0;
     }
-    else if (linha <= 0)
+    else if (linha == 0)
     {
         linha = 25;
     }
 
-    if (coluna >= 80)
+    if (coluna == 80)
     {
         coluna = 0;
     }
-    else if (coluna <= 0)
+    else if (coluna == 0)
     {
         coluna = 80;
     }
 }
 
-void inicializarMatriz()
-{
-
-    for (coluna = 0; coluna < 80; coluna++)
-    {
-        for (linha = 25; linha < 25; linha++)
-        {
-            guardaPosicao[coluna][linha] = 0;
-        }
-    }
-
-    // Para coluna e linha não começar com valores errados.
-    coluna = 1;
-    linha = 1;
-}
-
 void gameOver()
 {
-    for (coluna = 0; coluna < 80; coluna++)
+    for (coluna = 0; coluna <= 80; coluna++)
     {
-        for (linha = 0; linha < 25; linha++)
+        for (linha = 0; linha <= 25; linha++)
         {
             gotoxy(coluna, linha);
             printf(" ");
@@ -90,8 +73,6 @@ int main()
     signed char incrementoColuna = 1;
     gotoxy(1,1);
     printf("*");
-
-    inicializarMatriz();
 
     for(;;) // Loop infinito.
     {
@@ -128,20 +109,16 @@ int main()
 
         // Primeiro guarda a posição e depois altera a coluna e linha.
         guardaPosicao[coluna][linha] = 1;
-        gotoxy(coluna+=incrementoColuna, linha+=incrementoLinha);
+        gotoxy(coluna += incrementoColuna, linha += incrementoLinha);
 
         antiEscape();
 
         if (guardaPosicao[coluna][linha] == 1)
         {
             gameOver();
-            break; // Na verdade, esse break não precisa porque fica em um loop infinito no pisca-pisca "GAME OVER!".
         }
-        else
-        {
-            Sleep(100); // Faz o jogo não ficar rápido.
-            printf("*");
-        }
+        Sleep(100); // Faz o jogo não ficar rápido.
+        printf("*");
     }
     return 0;
 }
